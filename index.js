f3015435ec6a194b2a3133948f4ebe35bc0139a5
somesh07 <somesh2711@gmail.com>
2018-07-03 18:38:36 +0530

    my fifth commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".success").on("click" ,function(){
+        $(".modal").css("display", "flex");
+            
+        });
+
+    });
+
+$(document).ready(function(){
+    $(".close").on("click" ,function(){
+        $(".modal").hide();
+            
+        });
+    
+    });
